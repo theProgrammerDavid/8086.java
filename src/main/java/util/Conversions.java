3b95f6c32b3ad2059f320b3f310baee4e733039f
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-08-31 00:45:00 +0530

    feat: conversions

---
+++ src/main/java/util/Conversions.java
@@ -0,0 +1,48 @@
+package util;
+
+public class Conversions {
+  public static final double log2 = Math.log(2);
+
+  /**
+   *
+   * @param value String String representation of number to be converted
+   * @return int[2] - index 0 is the converted value in int, index 1 is the size
+   *     in bits of the binary representation
+   * @throws Exception, NumberFormatException
+   */
+  public static int[] toNumber(final String value) throws Exception, NumberFormatException {
+    int initVal = 0;
+    if (value.startsWith("0B")) {
+      initVal = Integer.parseInt(value, 2);
+    } else if (value.startsWith("0X")) {
+      initVal = Integer.parseInt(value, 16);
+    } else if (value.startsWith("0")) {
+      initVal = Integer.parseInt(value, 8);
+    } else {
+      initVal = Integer.parseInt(value, 10);
+    }
+
+    final int size =
+        (int)Math.ceil(Math.log((double)initVal) / Conversions.log2);
+    if (initVal > 65536) {
+      throw new Exception(
+          "Value too large to convert. Only 16 bit numbers allowed");
+    }
+
+    int[] retVal = new int[2];
+    retVal[0] = initVal;
+    retVal[1] = size;
+    return retVal;
+  }
+
+  public static final int getSize(final int initVal) throws Exception {
+    final int size =
+        (int)Math.ceil(Math.log((double)initVal) / Conversions.log2);
+    if (initVal > 65536) {
+      throw new Exception(
+          "Value too large to convert. Only 16 bit numbers allowed");
+    }
+
+    return size;
+  }
+}
