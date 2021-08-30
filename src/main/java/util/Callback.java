e2f470845acda08438c33d64bebaec17eb601981
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-08-30 18:37:23 +0530

    docs: comments

--- src/main/java/util/Callback.java
+++ src/main/java/util/Callback.java
@@ -1,5 +1,9 @@
 package util;
 
+/**
+ * Interface for creating named functions
+ * generally used with ActionEvent Handlers
+ */
 public interface Callback {
   public void call();
 }
--- src/main/java/util/NamedFunction.java
+++ src/main/java/util/NamedFunction.java
@@ -1,15 +1,40 @@
 package util;
 
+/**
+ * Class to Instantiate ActionEvents and Event Handlers
+ * Instead of using java.util.function.Function<T,R>, this uses util.Callback 
+ * to create an interface which we then use to instantiate EventHandlers with 
+ * the appropriate events.
+ * 
+ * eg. 
+ * NamedFunction nf(...);
+ * node.setOnAction((event) -> { nf.callFunction(); });
+ */
 public class NamedFunction {
   private String name;
   private Callback fn;
+
+/**
+ * @constructor
+ * @param name String
+ * @param callback util.Callback
+ */
 
   public NamedFunction(final String name, Callback callback) {
     this.name = name;
     this.fn = callback;
   }
 
+
+  /**
+   * Returns name of the NamedFunction instance
+   * @return String
+   */
   public String getName() { return this.name; }
 
+  /**
+   * Blocking function call
+   * @return void
+   */
   public void callFunction() { fn.call(); }
 }
