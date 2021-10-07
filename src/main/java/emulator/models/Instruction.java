bee8a464dfeb7a2041f8dc7317ea51dd65b03ddf
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-10-08 00:58:21 +0530

    feat: Instruction toString for debug

--- src/main/java/emulator/models/Instruction.java
+++ src/main/java/emulator/models/Instruction.java
@@ -17,4 +17,18 @@ public class Instruction {
     this.op1 = op1;
     this.op2 = op2;
   }
+
+  /**
+   * @return String 
+   */
+  public String toString() {
+    // System.out.printf("[Mnemonic: name:%s value:%s]\t[Operand %s
+    // %s]\t[Operand %s %s]\n", mnemonic.name, mnemonic.value, op1.name,
+    // op1.value,
+    //                   op2.name, op2.value);
+    return String.format(
+        "[Mnemonic: name:%s value:%s]\t[Operand %s %s]\t[Operand %s %s]",
+        mnemonic.name, mnemonic.value, op1.name, op1.value, op2.name,
+        op2.value);
+  }
 }
